90fc4245f776677ff7822ba8e0c5e31776d3f6fe
Muthu <muthu.r@digiapt.com>
2021-03-04 11:01:37 +0530

    jobs api optimization

--- controller/jobs.server.controller.js
+++ controller/jobs.server.controller.js
@@ -11,7 +11,7 @@ exports.getjob = function(req, res) {
     }
 
     condition = utility.getFilterCondition(req.body.filter)
-    NaukriPostedJob.find(condition,
+    NaukriPostedJob.find(condition, {_id: 1, company_name: 1, company_address: 1, employment_type: 1, role: 1},
         function(err, docs) {
             if (err) {
                 res.send(err)
